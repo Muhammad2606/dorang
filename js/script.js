b1238ce426aaf0ad681da730f3f4ab9a19e884db
Muhammad <muhammadibrohimov0306@gmail.com>
2023-05-01 20:14:39 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+window.addEventListener('DOMContentLoaded', () =>{
+
+const search = document.querySelector('.fa-search'),
+      watchBtn = document.querySelector('[data-watch]'),
+      watch = document.querySelector('.watch'),
+      inputSearch = document.querySelector('.add__search'),
+      header = document.querySelector('.header'),
+      navigation = document.querySelector('.navigation');
+     
+    
+    search.addEventListener('click', () =>{
+        inputSearch.classList.add('watch__ani','show');
+        inputSearch.classList.remove('hide');
+
+       
+    });
+     header.addEventListener('click', (e) =>{
+        const target = e.target;
+        if(target && target.classList.contains('fa-search')){
+        inputSearch.classList.add('watch__ani','show');
+        }
+        else{
+        inputSearch.classList.add('hide');
+        inputSearch.classList.remove('watch__ani','show');
+        }
+    });
+    watchBtn.addEventListener('click', () =>{
+        watch.classList.add('watch__ani', 'show');
+        watch.classList.remove('hide');
+    });
+    watch.addEventListener('click', (e) =>{
+        if(e.target == watch){
+        watch.classList.add('hide');
+        watch.classList.remove('watch__ani', 'show');
+        }
+    });
+
+    // function byScroll () {
+    //     if(window.pageYOffset == 0 ){
+    //         hideColor();
+    //     }
+    //     else{
+    //         showColor();
+    //     }
+    //   }
+    //   byScroll();
+    //   window.addEventListener('scroll', byScroll);
+
+      function byScroll (){
+        if(window.pageYOffset == 0){
+            navigation.classList.remove('add__navigation__color');
+        }
+        else{
+            navigation.classList.add('add__navigation__color');
+
+        }
+        
+      }
+ byScroll();
+ window.addEventListener('scroll', byScroll);
+      
+ 
+});
